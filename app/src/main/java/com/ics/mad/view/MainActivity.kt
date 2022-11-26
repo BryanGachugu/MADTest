1e21b80bcc785c1600b398d8b7136f3e9bb49f3c
BryanGachugu <briangachugu5@gmail.com>
2022-11-26 16:04:11 +0300

    changes

--- app/src/main/java/com/ics/mad/view/MainActivity.kt
+++ app/src/main/java/com/ics/mad/view/MainActivity.kt
@@ -2,13 +2,23 @@ package com.ics.mad.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.ics.mad.R
+import com.ics.mad.databinding.ActivityMainBinding
+import com.ics.mad.view.adapter.ResourcesAdapter
+import com.ics.mad.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.viewmodel = mainViewModel
+        binding.adapter = mainViewModel.codingResources.value?.let { ResourcesAdapter(it) }
 
 
     }
